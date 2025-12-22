8ce6c2cf48935cec5a793d30697f2605e9ad7662
ronnygptflorian-hue <ronnygptflorian@gmail.com>
2025-12-21 20:21:40 -0400

    Updated crear-factura.component.ts

--- src/app/components/facturas/crear-factura.component.ts
+++ src/app/components/facturas/crear-factura.component.ts
@@ -274,9 +274,30 @@ export class CrearFacturaComponent implements OnInit {
   }
 
   onClientInputFocus() {
-    if (!this.clientLocked && this.form.value.clientSearch?.length >= 2) {
-      this.showClientDropdown = true;
+    if (!this.clientLocked) {
+      if (this.form.value.clientSearch?.length >= 2) {
+        this.showClientDropdown = true;
+      } else {
+        this.loadInitialClients();
+      }
     }
+  }
+
+  loadInitialClients() {
+    this.isSearchingClient = true;
+    this.clientError = '';
+    this.showClientDropdown = true;
+
+    this.customerService.getClientes(1, 2, {}).subscribe({
+      next: (resp) => {
+        this.clientResults = resp.data;
+        this.isSearchingClient = false;
+      },
+      error: (error) => {
+        this.clientError = 'Error al cargar clientes';
+        this.isSearchingClient = false;
+      }
+    });
   }
 
   onClientTyping() {
@@ -334,7 +355,26 @@ export class CrearFacturaComponent implements OnInit {
   onProductInputFocus() {
     if (this.form.value.productSearch?.length >= 2) {
       this.showProductDropdown = true;
+    } else {
+      this.loadInitialProducts();
     }
+  }
+
+  loadInitialProducts() {
+    this.isSearchingProduct = true;
+    this.productError = '';
+    this.showProductDropdown = true;
+
+    this.productService.getProducts(1, 2, {}).subscribe({
+      next: (resp) => {
+        this.productResults = resp.data;
+        this.isSearchingProduct = false;
+      },
+      error: (error) => {
+        this.productError = 'Error al cargar productos';
+        this.isSearchingProduct = false;
+      }
+    });
   }
 
   onProductTyping() {
