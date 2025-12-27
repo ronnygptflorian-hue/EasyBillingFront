cb198461344e3727aad7e582a71716bda4f45844
ronnygptflorian-hue <ronnygptflorian@gmail.com>
2025-12-27 12:04:17 -0400

    Updated feature.service.ts

--- src/app/components/configuraciones/service/feature.service.ts
+++ src/app/components/configuraciones/service/feature.service.ts
@@ -13,18 +13,18 @@ export class FeatureService {
   private apiUrl = enviroment.apiBaseUrl;
 
   getAllFeatures(): Observable<ApiResponse<Feature[]>> {
-    return this.http.get<ApiResponse<Feature[]>>(`${this.apiUrl}/CoreConfig/feature/all`);
+    return this.http.get<ApiResponse<Feature[]>>(`${this.apiUrl}CoreConfig/feature/all`);
   }
 
   getEmpresaFeatures(idEmpresa: number): Observable<ApiResponse<EmpresaFeature[]>> {
-    return this.http.get<ApiResponse<EmpresaFeature[]>>(`${this.apiUrl}/CoreConfig/empresa-feature/by-empresa/${idEmpresa}`);
+    return this.http.get<ApiResponse<EmpresaFeature[]>>(`${this.apiUrl}CoreConfig/empresa-feature/by-empresa/${idEmpresa}`);
   }
 
   addFeatureToEmpresa(request: EmpresaFeatureRequest): Observable<ApiResponse<EmpresaFeature>> {
-    return this.http.post<ApiResponse<EmpresaFeature>>(`${this.apiUrl}/CoreConfig/empresa-feature`, request);
+    return this.http.post<ApiResponse<EmpresaFeature>>(`${this.apiUrl}CoreConfig/empresa-feature`, request);
   }
 
   deleteEmpresaFeature(id: number): Observable<ApiResponse<any>> {
-    return this.http.delete<ApiResponse<any>>(`${this.apiUrl}/CoreConfig/empresa-feature/${id}`);
+    return this.http.delete<ApiResponse<any>>(`${this.apiUrl}CoreConfig/empresa-feature/${id}`);
   }
 }
